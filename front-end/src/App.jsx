a426f4ba494584360c0a1f9e3724f9d58986a560
Tayshaun M. Delos Santos <tayshaunds25@gmail.com>
2025-02-05 08:57:11 +0800

    Created front end page template
    Setup the front end page template with Vite, React, and Tailwind CSS.

---
+++ front-end/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      {/* Navbar */}
+      <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto">
+          <h1 className="text-2xl font-bold text-white">Movie Recommendation System</h1>
+        </div>
+      </nav>
+
+      {/* Main Content Area */}
+      <div className="container mx-auto px-4 py-8">
+        {/* Content will go here */}
+      </div>
+    </div>
+  )
+}
+
+export default App
